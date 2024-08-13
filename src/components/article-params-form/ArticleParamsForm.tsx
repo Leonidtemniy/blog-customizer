f973cf2da8e2603082a2d2884fb5c34f2b9c1a7a
Leonid Temniy <leonidtemniy@yandex.ru>
2024-08-13 22:00:58 +0300

    реализовано открытие и закрытие саййдбара

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	//отслеживаем состояние закрыто/открыто с useState//
+	const [isOpen, setIsOpen] = useState(false);
+	//функция переключения  состояний//
+	const handleArrowButtonClick = () => {
+		setIsOpen((prevState) => !prevState);
+	};
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={handleArrowButtonClick} isOpen={isOpen} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
